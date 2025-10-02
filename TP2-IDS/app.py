c2a4c2eb603d5b930ef7d879181751323e8709c3
jchamorro-ai <jchamorro@fi.uba.ar>
2025-10-02 15:49:00 -0300

    renderizar template de inscripcion

--- TP2-IDS/app.py
+++ TP2-IDS/app.py
@@ -18,9 +18,11 @@ info_evento =  {
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/registration")
+@app.route("/registration", methods=["GET", "POST"])
 def registration():
-    return "<h1>Registration Page</h1>"
+    if request.method == "POST":
+        pass
+    return render_template("registration.html")
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port="5002", debug=True)
